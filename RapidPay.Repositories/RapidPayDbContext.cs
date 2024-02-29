3ebd4434f847c35b0083858609b82652fd90966f
Maximiliano Prystupiuk <mprystupiuk@gmail.com>
2024-02-29 12:35:32 -0300

    Card numbers unique per card

--- RapidPay.Repositories/RapidPayDbContext.cs
+++ RapidPay.Repositories/RapidPayDbContext.cs
@@ -10,7 +10,23 @@ namespace RapidPay.Data
 		protected override void OnModelCreating(ModelBuilder model)
 		{
 			model.HasDefaultSchema("RapidPay");
-			model.Entity<Card>().HasData(new Card() { Id = Guid.NewGuid(), Numbers = "123456789012345" });
+			model.Entity<Card>()
+				.HasIndex(e => e.Numbers)
+				.IsUnique();
+			
+			model.Entity<Card>()
+				.HasData(
+				new Card()
+				{
+					Id = Guid.NewGuid(),
+					Numbers = "123456789012345",
+					CardHolder = "Maxi",
+					CVV = 123,
+					ExpirationMonth = 1,
+					ExpirationYear = 2027,
+					Balance = 10000
+				}
+				);
 		}
 
 	}
